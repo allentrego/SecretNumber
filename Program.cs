fa7e440350c807e45cc0c150025b52bfa8341bb2
Allen Trego <90735642+allentrego@users.noreply.github.com>
2025-05-24 11:46:33 -0400

    Prompt player and read guess

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿
-
+// generate a random number between 1 and 100
 int randomNumber = new Random().Next(1, 101);
 
+// ask the player to guess the number
+Console.WriteLine("Guess a number between 1 and 100: ");
+
+// read the player's guess
+int playerGuess = Convert.ToInt32(Console.ReadLine());
 
