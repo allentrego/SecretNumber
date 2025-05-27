730748ffdf00a5931f5e318e297e48b7d9e572b6
Allen Trego <90735642+allentrego@users.noreply.github.com>
2025-05-27 13:21:02 -0400

    Added game loop and congratulation message

--- Program.cs
+++ Program.cs
@@ -8,3 +8,16 @@ Console.WriteLine("Guess a number between 1 and 100: ");
 // read the player's guess
 int playerGuess = Convert.ToInt32(Console.ReadLine());
 
+
+// start the game loop
+while (playerGuess != randomNumber)
+{
+
+    Console.WriteLine("Wrong guess! Try again: ");
+    playerGuess = Convert.ToInt32(Console.ReadLine());
+
+}
+// player guessed the number correctly
+
+Console.WriteLine("Congratulations! You guessed the number: " + randomNumber);
+
